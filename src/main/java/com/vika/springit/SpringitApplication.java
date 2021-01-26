5f4f1a6fe2aac2ed821bf14087bcaf95d256716e
VictoriaRaitses <victoria.raitses@gmail.com>
2021-01-26 15:54:25 +0100

    Updated application properties with some basic h2 & datasource setup

--- src/main/java/com/vika/springit/SpringitApplication.java
+++ src/main/java/com/vika/springit/SpringitApplication.java
@@ -2,10 +2,8 @@ package com.vika.springit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringitApplication {
@@ -16,14 +14,4 @@ public class SpringitApplication {
         SpringApplication.run(SpringitApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner runner(){
-        return args -> {
-            log.error("CommandLineRunner.run();");
-            log.warn("CommandLineRunner.run();");
-            log.info("CommandLineRunner.run();");
-            log.debug("CommandLineRunner.run();");
-            log.trace("CommandLineRunner.run();");
-        };
-    }
 }
