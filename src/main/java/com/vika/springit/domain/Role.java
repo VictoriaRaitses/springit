7757b4ed3b1a69fe87746c0ec65e47b7faf75f52
VictoriaRaitses <victoria.raitses@gmail.com>
2021-02-11 18:07:16 +0100

    spring-security

---
+++ src/main/java/com/vika/springit/domain/Role.java
@@ -0,0 +1,30 @@
+package com.vika.springit.domain;
+
+import io.micrometer.core.lang.NonNull;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Collection;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Role {
+
+
+        @Id
+        @GeneratedValue
+        private Long id;
+
+        @NonNull
+        private String name;
+
+        @ManyToMany( mappedBy = "roles")
+        private Collection<User> users;
+
+    }
---
+++ src/main/java/com/vika/springit/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.vika.springit.repository;
+
+import com.vika.springit.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
