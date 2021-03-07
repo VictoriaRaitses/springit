3d7421dba701899710804f1fb12b1b6088ac57c9
VictoriaRaitses <victoria.raitses@gmail.com>
2021-03-07 17:40:57 +0100

    page activation

--- src/main/java/com/vika/springit/sevice/UserService.java
+++ src/main/java/com/vika/springit/sevice/UserService.java
@@ -4,21 +4,46 @@ import com.vika.springit.domain.User;
 import com.vika.springit.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
 
     private final Logger logger = LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
+    private final RoleService roleService;
+    private final MailService mailService;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, RoleService roleService, MailService mailService) {
         this.userRepository = userRepository;
+        this.roleService = roleService;
+        this.mailService = mailService;
+        encoder = new BCryptPasswordEncoder();
     }
 
     public User register(User user) {
+        // take the password from the form and encode
+        String secret = "{bcrypt}" + encoder.encode(user.getPassword());
+        user.setPassword(secret);
+        // confirm password
+        user.setConfirmPassword(secret);
+        // assign a role to this user
+        user.addRole(roleService.findByName("ROLE_USER"));
+        // set an activation code
+        user.setActivationCode(UUID.randomUUID().toString());
+        // disable the user
+
+        // save user
+        save(user);
+        // send the activation email
+//        sendActivationEmail(user);
+        // return the user
         return user;
     }
 
@@ -32,6 +57,18 @@ public class UserService {
             logger.info("Saving User: " + user.getEmail());
             userRepository.save(user);
         }
+    }
+
+    public void sendActivationEmail(User user) {
+        mailService.sendActivationEmail(user);
+    }
+
+    public void sendWelcomeEmail(User user) {
+        mailService.sendWelcomeEmail(user);
+    }
+
+    public Optional<User> findByEmailAndActivationCode(String email, String activationCode) {
+        return userRepository.findByEmailAndActivationCode(email,activationCode);
     }
 
 }
